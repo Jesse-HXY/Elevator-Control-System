cd6f5d8810bde30598801f8d4d4612b7d60b8dbe
hzh <972802149@qq.com>
2018-12-26 14:24:53 +0800

    完成大部分test

--- src/main/test/com/neu/test.java
+++ src/main/test/com/neu/test.java
@@ -3,10 +3,13 @@ package com.neu;
 import com.neu.listener.ElevatorController;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class test {
-    @Test
+    /*@Test
     public void testmoving() {
         ElevatorController elevatorController = ElevatorController.getInstance();
         elevatorController.floorButtonPressed(3);
@@ -22,6 +25,50 @@ public class test {
         elevatorController.openButtonPressed();
         elevatorController.closedButtonPressed();
         assertEquals(1, elevatorController.getCurrentFloorNum());
+    }*/
+
+    @Test
+    public void testPressnNumberButton(){
+        ElevatorController elevatorController = ElevatorController.getInstance();
+        elevatorController.setCurrentFloorNum(3);
+        elevatorController.floorButtonPressed(4);
+        elevatorController.floorButtonPressed(5);
+        elevatorController.floorButtonPressed(1);
+        elevatorController.floorButtonPressed(2);
+        List<Double> upList = new ArrayList<>();
+        List<Double> downList = new ArrayList<>();
+        downList.add(1.0);
+        downList.add(2.0);
+        upList.add(4.0);
+        upList.add(5.0);
+        assertEquals(upList, elevatorController.getUpList());
+        assertEquals(downList, elevatorController.getDownList());
+    }
+
+    @Test
+    public void testOpenPress(){
+        ElevatorController elevatorController = ElevatorController.getInstance();
+        elevatorController.openButtonPressed();
+        assertEquals(elevatorController.getDoorOpen(), elevatorController.getCurrentState());
+    }
+
+    @Test
+    public void testClosePress(){
+        ElevatorController elevatorController = ElevatorController.getInstance();
+        elevatorController.floorButtonPressed(4);
+        elevatorController.openButtonPressed();
+        elevatorController.closedButtonPressed();
+        assertEquals(elevatorController.getMovingUp(), elevatorController.getCurrentState());
+    }
+
+    @Test
+    public void testMoving(){
+        ElevatorController elevatorController = ElevatorController.getInstance();
+        elevatorController.floorButtonPressed(2);
+        elevatorController.openButtonPressed();
+        elevatorController.closedButtonPressed();
+        assertEquals(true, elevatorController.moving());
+        assertEquals(false, elevatorController.moving());
     }
 
 }
